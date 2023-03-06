c002185b431fb2fbcb06a98215796fcda1339b7d
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-03-06 15:11:41 +0100

    La til ny spraakfil som skal erstatte de gamle, inkluderer nå også NN og EN

---
+++ src/language/text.jsx
@@ -0,0 +1,72 @@
+export const text = {
+  varsel: {
+    nb: "Varsel",
+    nn: "Varsel",
+    en: "Notification",
+  },
+  varselIngressOppgaveEntall: {
+    nb: "{antallOppgaver} oppgave",
+    nn: "{antallOppgaver} oppgåve",
+    en: "{antallOppgaver} task",
+  },
+  varselIngressOppgaveFlertall: {
+    nb: "{antallOppgaver} oppgaver",
+    nn: "{antallOppgaver} oppgåver",
+    en: "{antallOppgaver} tasks",
+  },
+  varselIngressBeskjedEntall: {
+    nb: "{antallBeskjeder} beskjed",
+    nn: "{antallBeskjeder} beskjed",
+    en: "{antallBeskjeder} message",
+  },
+  varselIngressBeskjedFlertall: {
+    nb: "{antallBeskjeder} beskjeder",
+    nn: "{antallBeskjeder} beskjeder",
+    en: "{antallBeskjeder} messages",
+  },
+  varselOppgaverOgBeskjeder: {
+    nb: " og ",
+    nn: " og ",
+    en: " and ",
+  },
+  varselIngressIngenVarsler: {
+    nb: "Ingen varsler",
+    nn: "Ingen varsel",
+    en: "No notifications",
+  },
+  utkastTittel: {
+    nb: "Utkast",
+    nn: "Utkast",
+    en: "Drafts",
+  },
+  utkastIngressEntall: {
+    nb: "En påbegynt søknad",
+    nn: "Ei påbyrja søknad",
+    en: "One started application",
+  },
+  utkastIngressFlertall: {
+    nb: "{antall} påbegynte søknader",
+    nn: "{antall} påbyrja søknadar",
+    en: "{antall} started applications",
+  },
+  sidetittelVelkomsthilsen: {
+    nb: "Hei, ",
+    nn: "Hei, ",
+    en: "Hi, ",
+  },
+  sidetittelVelkomsthilsenMorgen: {
+    nb: "God morgen, ",
+    nn: "God morgon, ",
+    en: "Good morning, ",
+  },
+  sidetittelVelkomsthilsenKveld: {
+    nb: "God kveld, ",
+    nn: "God kveld, ",
+    en: "Good evening, ",
+  },
+  loaderTekst: {
+    nb: "Laster...",
+    nn: "Lastar...",
+    en: "Loading...",
+  },
+};
