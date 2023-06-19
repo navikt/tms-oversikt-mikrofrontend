a4153350da186d5ea623a2a1bcaf9842f5042216
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-06-19 14:09:40 +0200

    Refaktorert navngivingen av innloggede-tjenester

--- src/components/innloggede-tjenester/InnloggedeTjenester.tsx
+++ src/components/innloggede-tjenester/InnloggedeTjenester.tsx
@@ -4,7 +4,7 @@ import { useTranslate } from "../../hooks/useTranslate";
 import { BodyShort, Detail, Heading } from "@navikt/ds-react";
 import { LanguageContext } from "../../language/LanguageProvider";
 import LenkeListe from "./LenkeListe";
-import style from "./InnloggedeTjenester.module.css";
+import styles from "./InnloggedeTjenester.module.css";
 
 const InnloggedeTjenester = () => {
   const language = useContext(LanguageContext);
@@ -12,15 +12,15 @@ const InnloggedeTjenester = () => {
   const isEnglish = language === "en";
 
   return (  
-    <div className={style.background}>
-      <Heading level="2" size="small" className={style.tittel}>{useTranslate("innloggedeTjenesterTittel")}</Heading>
-      {isEnglish ? <BodyShort size="medium" className={style.disclaimer}>{useTranslate("alleTjenesterDisclaimer")}</BodyShort> : null}
-      <nav className={style.container}>
-        <div className={style.listeContainer}>
+    <div className={styles.container}>
+      <Heading level="2" size="small" className={styles.tittel}>{useTranslate("innloggedeTjenesterTittel")}</Heading>
+      {isEnglish ? <BodyShort size="medium" className={styles.disclaimer}>{useTranslate("alleTjenesterDisclaimer")}</BodyShort> : null}
+      <nav className={styles.contentContainer}>
+        <div className={styles.listColumn}>
           <LenkeListe tittel={useTranslate("jobbOgOppfolgingTittel")} liste={jobb}/>
           <LenkeListe tittel={useTranslate("pengeStøtteOgHjelpemidlerTittel")} liste={hjelpemidler}/>
         </div>
-        <div className={style.listeContainer}>
+        <div className={styles.listColumn}>
           <LenkeListe tittel={useTranslate("personopplysningTittel")} liste={personopplysning}/>
           <LenkeListe tittel={useTranslate("annetTittel")} liste={annet}/>
         </div>
