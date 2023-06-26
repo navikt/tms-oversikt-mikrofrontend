4bf2d2147f1d5c7300fdb31c45fce203cef6bcfd
Eivinn Eilertsen <eivinne@Eivinns-MacBook-Pro.local>
2023-06-26 10:43:33 +0200

    Lagt til funksjonalitet for å skille mellom prod og dev lenker i innloggede-tjenester lenkene. Oppdatert lenke til Yrkesskade og yrkessykdom i prod og dev.

--- src/components/innloggede-tjenester/Lenker.ts
+++ src/components/innloggede-tjenester/Lenker.ts
@@ -1,4 +1,18 @@
-export const jobb = [
+const isProduction = window.location.href.includes("www.nav.no");
+const isDevelopment = window.location.href.includes("www.intern.dev.nav.no");
+
+export const getEnvironment = () => {
+  if (isProduction) {
+    return "production";
+  }
+
+  if (isDevelopment) {
+    return "development";
+  }
+  return "local";
+};
+
+const jobbLenkerProd = [
   {
     nb: "Aktivitetsplan",
     nn: "Aktivitetsplan",
@@ -71,7 +85,80 @@ export const jobb = [
   },
 ];
 
-export const hjelpemidler = [
+const jobbLenkerDev = [
+  {
+    nb: "Aktivitetsplan",
+    nn: "Aktivitetsplan",
+    en: "Activity plan",
+    url: {
+      nb: "https://aktivitetsplan.nav.no/",
+      nn: "https://aktivitetsplan.nav.no/",
+      en: "https://aktivitetsplan.nav.no/",
+    },
+  },
+  {
+    nb: "CV til bruk ved jobbsøk (Arbeidsplassen)",
+    nn: "CV",
+    en: "CV",
+    url: {
+      nb: "https://arbeidsplassen.nav.no/minside",
+      nn: "https://arbeidsplassen.nav.no/minside",
+      en: "https://arbeidsplassen.nav.no/minside",
+    },
+  },
+  {
+    nb: "Dialog med din lokale veileder",
+    nn: "Dialog med den lokale rettleiaren din",
+    en: "Communication with your local consultant",
+    url: {
+      nb: "http://nav.no/arbeid/dialog",
+      nn: "http://nav.no/arbeid/dialog",
+      en: "http://nav.no/arbeid/dialog",
+    },
+  },
+  {
+    nb: "Jobbsporet ditt",
+    nn: "Jobbsporet ditt",
+    en: "Job track",
+    url: {
+      nb: "https://jobbsporet.nav.no ",
+      nn: "https://jobbsporet.nav.no ",
+      en: "https://jobbsporet.nav.no ",
+    }
+  },
+  {
+    nb: "Ledige stillinger (Arbeidsplassen)",
+    nn: "Ledige stillingar (Arbeidsplassen)",
+    en: "Job listings (Arbeidsplassen)",
+    url: {
+      nb: "https://arbeidsplassen.nav.no/stillinger",
+      nn: "https://arbeidsplassen.nav.no/stillinger",
+      en: "https://arbeidsplassen.nav.no/stillinger",
+    }
+  },
+  {
+    nb: "Meldekort",
+    nn: "Meldekort",
+    en: "Employment status form",
+    url: {
+      nb: "https://www.nav.no/meldekort/",
+      nn: "https://www.nav.no/meldekort/",
+      en: "https://www.nav.no/meldekort/",
+    }
+  },
+  {
+    nb: "Registrer deg som arbeidssøker",
+    nn: "Registrer deg som arbeidssøkar",
+    en: "Register as a job seeker",
+    url: {
+      nb: "https://arbeidssokerregistrering.nav.no/",
+      nn: "https://arbeidssokerregistrering.nav.no/",
+      en: "https://arbeidssokerregistrering.nav.no/",
+    }
+  },
+];
+
+const hjelpemidlerLenkerProd = [
   {
     nb: "AAP (arbeidsavklaringspenger)",
     nn: "AAP (abeidsavklaringspengar)",
@@ -153,6 +240,16 @@ export const hjelpemidler = [
     }
   },
   {
+    nb: "Yrkesskade og yrkessykdom",
+    nn: "Yrkesskade og yrkessjukdom",
+    en: "Occupational injury and occupational illness",
+    url: {
+      nb: "https://yrkesskadeoversikt.nav.no/",
+      nn: "https://yrkesskadeoversikt.nav.no/",
+      en: "https://yrkesskadeoversikt.nav.no/",
+    }
+  },
+  {
     nb: "Økonomisk sosialhjelp",
     nn: "Økonomisk sosialhjelp",
     en: "Financial assistance",
@@ -164,7 +261,110 @@ export const hjelpemidler = [
   },
 ];
 
-export const personopplysning = [
+const hjelpemidlerLenkerDev = [
+  {
+    nb: "AAP (arbeidsavklaringspenger)",
+    nn: "AAP (abeidsavklaringspengar)",
+    en: "Work assessment allowance (AAP)",
+    url: {
+      nb: "https://www.nav.no/aap/mine-aap/",
+      nn: "https://www.nav.no/aap/mine-aap/nn/",
+      en: "https://www.nav.no/aap/mine-aap/",
+    }
+  },
+  {
+    nb: "Dagpenger",
+    nn: "Dagpengar ",
+    en: "Unemployment benefits (dagpenger)",
+    url: {
+      nb: "https://www.nav.no/arbeid/dagpenger/mine-dagpenger",
+      nn: "https://www.nav.no/arbeid/dagpenger/mine-dagpenger",
+      en: "https://www.nav.no/arbeid/dagpenger/mine-dagpenger",
+    }
+  },
+  {
+    nb: "Foreldrepenger",
+    nn: "Foreldrepengar",
+    en: "Parental benefit",
+    url: {
+      nb: "https://foreldrepenger.nav.no/",
+      nn: "https://foreldrepenger.nav.no/",
+      en: "https://foreldrepenger.nav.no/",
+    }
+  },
+  {
+    nb: "Hjelpemidler",
+    nn: "Hjelpemiddel",
+    en: "Assistive technology",
+    url: {
+      nb: "https://www.nav.no/hjelpemidler/dinehjelpemidler/",
+      nn: "https://www.nav.no/hjelpemidler/dinehjelpemidler/",
+      en: "https://www.nav.no/hjelpemidler/dinehjelpemidler/",
+    }
+  },
+  {
+    nb: "Pleiepenger",
+    nn: "Pleiepengar",
+    en: "Attendance allowance",
+    url: {
+      nb: "https://www.nav.no/familie/sykdom-i-familien/soknad/innsyn",
+      nn: "https://www.nav.no/familie/sykdom-i-familien/soknad/innsyn",
+      en: "https://www.nav.no/familie/sykdom-i-familien/soknad/innsyn",
+    }
+  },
+  {
+    nb: "Pensjon",
+    nn: "Pensjon",
+    en: "Pension",
+    url: {
+      nb: "https://www.nav.no/pselv/publisering/dinpensjon.jsf?context=pensjon",
+      nn: "https://www.nav.no/pselv/publisering/dinpensjon.jsf?context=pensjon",
+      en: "https://www.nav.no/pselv/publisering/dinpensjon.jsf?context=pensjon",
+    }
+  },
+  {
+    nb: "Sykefravær",
+    nn: "Sjukefravær",
+    en: "Absence due to sickness",
+    url: {
+      nb: "https://www.nav.no/syk/sykefravaer",
+      nn: "https://www.nav.no/syk/sykefravaer",
+      en: "https://www.nav.no/syk/sykefravaer",
+    }
+  },
+  {
+    nb: "Uføretrygd",
+    nn: "Uføretrygd",
+    en: "Disability benefit",
+    url: {
+      nb: "https://www.nav.no/pselv/publisering/uforetrygd.jsf?context=ut",
+      nn: "https://www.nav.no/pselv/publisering/uforetrygd.jsf?context=ut",
+      en: "https://www.nav.no/pselv/publisering/uforetrygd.jsf?context=ut",
+    }
+  },
+  {
+    nb: "Yrkesskade og yrkessykdom",
+    nn: "Yrkesskade og yrkessjukdom",
+    en: "Occupational injury and occupational illness",
+    url: {
+      nb: "https://yrkesskadeoversikt.intern.dev.nav.no/",
+      nn: "https://yrkesskadeoversikt.intern.dev.nav.no/",
+      en: "https://yrkesskadeoversikt.intern.dev.nav.no/",
+    }
+  },
+  {
+    nb: "Økonomisk sosialhjelp",
+    nn: "Økonomisk sosialhjelp",
+    en: "Financial assistance",
+    url: {
+      nb: "https://www.nav.no/sosialhjelp/innsyn/",
+      nn: "https://www.nav.no/sosialhjelp/innsyn/",
+      en: "https://www.nav.no/sosialhjelp/innsyn/",
+    }
+  },
+];
+
+const personopplysningLenkerProd = [
   {
     nb: "Adresse (se og endre)",
     nn: "Adresse (sjå og endre)",
@@ -257,7 +457,100 @@ export const personopplysning = [
   },
 ];
 
-export const annet = [
+const personopplysningLenkerDev = [
+  {
+    nb: "Adresse (se og endre)",
+    nn: "Adresse (sjå og endre)",
+    en: "Addresses",
+    url: {
+      nb: "https://www.nav.no/person/personopplysninger/nb/#adresser",
+      nn: "https://www.nav.no/person/personopplysninger/nn/#adresser",
+      en: "https://www.nav.no/person/personopplysninger/en/#adresser",
+    }
+  },
+  {
+    nb: "Arbeidsforhold",
+    nn: "Arbeidsforhold",
+    en: "Employment relationship",
+    url: {
+      nb: "https://www.nav.no/person/personopplysninger/nb/#arbeidsforhold",
+      nn: "https://www.nav.no/person/personopplysninger/nn/#arbeidsforhold",
+      en: "https://www.nav.no/person/personopplysninger/en/#arbeidsforhold",
+    }
+  },
+  {
+    nb: "Ditt NAV-kontor",
+    nn: "Ditt NAV-kontor",
+    en: "Your NAV office",
+    url: {
+      nb: "https://www.nav.no/person/personopplysninger/nb/#ditt-nav-kontor",
+      nn: "https://www.nav.no/person/personopplysninger/nn/#ditt-nav-kontor",
+      en: "https://www.nav.no/person/personopplysninger/en/#ditt-nav-kontor",
+    }
+  },
+  {
+    nb: "Fullmakt",
+    nn: "Fullmakt",
+    en: "Powers of attorney",
+    url: {
+      nb: "https://www.nav.no/person/pdl-fullmakt-ui",
+      nn: "https://www.nav.no/person/pdl-fullmakt-ui",
+      en: "https://www.nav.no/person/pdl-fullmakt-ui",
+    }
+  },
+  {
+    nb: "Institusjonsopphold",
+    nn: "Institusjonsopphald",
+    en: "Institutional stays",
+    url: {
+      nb: "https://www.nav.no/person/personopplysninger/nb/institusjonsopphold",
+      nn: "https://www.nav.no/person/personopplysninger/nn/institusjonsopphold",
+      en: "https://www.nav.no/person/personopplysninger/en/institusjonsopphold",
+    }
+  },
+  {
+    nb: "Kontaktinformasjon",
+    nn: "Kontaktinformasjon",
+    en: "Contact information",
+    url: {
+      nb: "https://www.nav.no/person/personopplysninger/nb/#kontaktinformasjon",
+      nn: "https://www.nav.no/person/personopplysninger/nn/#kontaktinformasjon",
+      en: "https://www.nav.no/person/personopplysninger/en/#kontaktinformasjon",
+    }
+  },
+  {
+    nb: "Kontonummer for utbetaling",
+    nn: "Kontonummer for utbetaling",
+    en: "Account number for payment",
+    url: {
+      nb: "https://www.nav.no/person/personopplysninger/nb/#utbetaling",
+      nn: "https://www.nav.no/person/personopplysninger/nn/#utbetaling",
+      en: "https://www.nav.no/person/personopplysninger/en/#utbetaling",
+    }
+  },
+  {
+    nb: "Medlemskap i folketrygden",
+    nn: "Medlemskap i folketrygda",
+    en: "Membership in the National Insurance Scheme",
+    url: {
+      nb: "https://www.nav.no/person/personopplysninger/nb/medlemskap-i-folketrygden",
+      nn: "https://www.nav.no/person/personopplysninger/nn/medlemskap-i-folketrygden",
+      en: "https://www.nav.no/person/personopplysninger/en/medlemskap-i-folketrygden",
+    }
+  },
+  {
+    nb: "Personalia",
+    nn: "Personalia",
+    en: "Personal data",
+    url: {
+      nb: "https://www.nav.no/person/personopplysninger/nb/#personalia",
+      nn: "https://www.nav.no/person/personopplysninger/nn/#personalia",
+      en: "https://www.nav.no/person/personopplysninger/en/#personalia",
+    }
+  },
+];
+
+const annetLenkerProd = [
   {
     nb: "Pensjonskalkulator",
     nn: "Pensjonskalkulator",
@@ -309,3 +602,85 @@ export const annet = [
     }
   },
 ];
+
+const annetLenkerDev = [
+  {
+    nb: "Pensjonskalkulator",
+    nn: "Pensjonskalkulator",
+    en: "Calculate pensions",
+    url: {
+      nb: "https://www.nav.no/pselv/simulering.jsf",
+      nn: "https://www.nav.no/pselv/simulering.jsf",
+      en: "https://www.nav.no/pselv/simulering.jsf",
+    }
+  },
+  {
+    nb: "Dokumentarkiv",
+    nn: "Dokumentarkiv",
+    en: "Documents",
+    url: {
+      nb: "https://person.nav.no/mine-saker",
+      nn: "https://person.nav.no/mine-saker",
+      en: "https://person.nav.no/mine-saker",
+    }
+  },
+  {
+    nb: "Siste utbetalinger",
+    nn: "Siste utbetalingar",
+    en: "Recent payments",
+    url: {
+      nb: "https://tjenester.nav.no/utbetalingsoversikt/",
+      nn: "https://tjenester.nav.no/utbetalingsoversikt/",
+      en: "https://tjenester.nav.no/utbetalingsoversikt/",
+    }
+  },
+  {
+    nb: "Svar og referater",
+    nn: "Svar og referat",
+    en: "Replies and summaries",
+    url: {
+      nb: "https://innboks.nav.no/",
+      nn: "https://innboks.nav.no/",
+      en: "https://innboks.nav.no/",
+    }
+  },
+  {
+    nb: "Søknad og skjema",
+    nn: "Søknad og skjema",
+    en: "Applications and forms",
+    url: {
+      nb: "https://www.nav.no/soknader",
+      nn: "https://www.nav.no/soknader",
+      en: "https://www.nav.no/soknader/en/person",
+    }
+  },
+];
+
+const jobbLenkerConfig = {
+  local: jobbLenkerDev,
+  development: jobbLenkerDev,
+  production: jobbLenkerProd,
+};
+
+const hjelpemidlerLenkerConfig = {
+  local: hjelpemidlerLenkerDev,
+  development: hjelpemidlerLenkerDev,
+  production: hjelpemidlerLenkerProd,
+};
+
+const personopplysningLenkerConfig = {
+  local: personopplysningLenkerDev,
+  development: personopplysningLenkerDev,
+  production: personopplysningLenkerProd,
+};
+
+const annetLenkerConfig = {
+  local: annetLenkerDev,
+  development: annetLenkerDev,
+  production: annetLenkerProd,
+};
+
+export const jobbLenker = jobbLenkerConfig[getEnvironment()];
+export const hjelpemidlerLenker = hjelpemidlerLenkerConfig[getEnvironment()];
+export const personopplysningLenker = personopplysningLenkerConfig[getEnvironment()];
+export const annetLenker = annetLenkerConfig[getEnvironment()];
