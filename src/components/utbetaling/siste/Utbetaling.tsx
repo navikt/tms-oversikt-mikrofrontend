399abd538bee18ea5a48dd0d73f3b9801c374c5c
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2023-08-21 17:46:11 +0200

    Forenkle navn

--- src/components/utbetaling/siste/Utbetaling.tsx
+++ src/components/utbetaling/siste/Utbetaling.tsx
@@ -9,46 +9,50 @@ import UtbetalingContainer from "../container/UtbetalingContainer";
 import UtbetalingYtelser from "../ytelser/UtbetalingYtelser";
 import { text } from "../text"
 import styles from "./Utbetaling.module.css";
+import { UtbetalingResponse } from "../types";
 
 const Utbetaling = () => {
   const language = useContext(LanguageContext);
-  const { data: utbetalinger, isLoading} = useSWRImmutable(utbetalingsoversiktApiUrl, fetcher);
+  const { data, isLoading} = useSWRImmutable<UtbetalingResponse>(utbetalingsoversiktApiUrl, fetcher);
 
   if (isLoading) {
     return null;
   }
 
-  if (!hasUtbetalinger(utbetalinger.utbetalteUtbetalinger)) {
+  if (!data) {
     return null;
   }
 
-  const sisteUtbetaling = utbetalinger.utbetalteUtbetalinger[0];
-  const sisteUtbetalingSummert = summerYtelser(sisteUtbetaling.underytelser, sisteUtbetaling.trekk);
+  if (!hasUtbetalinger(data.utbetalteUtbetalinger)) {
+    return null;
+  }
 
-  const sisteUtbetalingDato = formatToReadableDate(utbetalinger.utbetalteUtbetalinger[0].ytelse_dato);
-  const sisteUtbetalingKonto = utbetalinger.utbetalteUtbetalinger[0].kontonummer;
+  const sisteUtbetaling = data.utbetalteUtbetalinger[0];
+  const sum = summerYtelser(sisteUtbetaling.underytelser, sisteUtbetaling.trekk);
+  const dato = formatToReadableDate(sisteUtbetaling.ytelse_dato);
+  const konto = sisteUtbetaling.kontonummer;
 
   return (
     <>
       <UtbetalingContainer type="siste">
-        <div className={styles["utbetaling-heading"]}>
+        <div className={styles.heading}>
           <BodyShort>
             {text.tittel[language]}
           </BodyShort>
           <BodyShort>
-            <a className={styles["utbetaling-link"]} href={utbetalingsoversiktUrl}>
+            <a className={styles.link} href={utbetalingsoversiktUrl}>
               {text.alle[language]}
             </a>
           </BodyShort>
         </div>
         <Heading size="large">
-          {sisteUtbetalingSummert + " kr"}
+          {sum + " kr"}
         </Heading>
         <BodyLong>
-          {sisteUtbetalingDato} {text.konto[language]} {sisteUtbetalingKonto}
+          {dato} {text.konto[language]} {konto}
         </BodyLong>
       </UtbetalingContainer>
-      <UtbetalingYtelser ytelse={sisteUtbetaling.ytelse} utbetaling={sisteUtbetalingSummert}/>
+      <UtbetalingYtelser ytelse={sisteUtbetaling.ytelse} utbetaling={sum}/>
     </>
   );
 };
