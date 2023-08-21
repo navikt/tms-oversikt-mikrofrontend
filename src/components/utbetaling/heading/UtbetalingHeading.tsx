98fe7781eaad8126f3d8e02eec21ed6ca2affab2
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2023-08-21 19:52:41 +0200

    Legg til amplitude måling

--- src/components/utbetaling/heading/UtbetalingHeading.tsx
+++ src/components/utbetaling/heading/UtbetalingHeading.tsx
@@ -4,6 +4,7 @@ import { text } from "../text";
 import { utbetalingsoversiktUrl } from "../urls";
 import { LanguageContext } from "../../../language/LanguageProvider";
 import styles from "./UtbetalingHeading.module.css";
+import { logNavigereEvent } from "../../../utils/amplitude";
 
 const UtbetalingHeading = () => {
   const language = useContext(LanguageContext);
@@ -13,7 +14,10 @@ const UtbetalingHeading = () => {
       <BodyShort>
         {text.tittel[language]}
       </BodyShort>
-      <a className={styles.link} href={utbetalingsoversiktUrl}>
+      <a
+        className={styles.link} href={utbetalingsoversiktUrl}
+        onClick={() => logNavigereEvent("utbetaling-widget", "generell", "Se alle")}
+      >
         {text.alle[language]}
       </a>
     </div>
