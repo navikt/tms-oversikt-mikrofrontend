7ba70056abbdd0be98ea1d4f6773f44a97151af3
Atle Nordland <atle.nordland@nav.no>
2023-08-28 12:05:28 +0200

    gjøre innboks klikkbar, designoppdateringer

--- src/components/innboks/Innboks.tsx
+++ src/components/innboks/Innboks.tsx
@@ -3,7 +3,7 @@ import { BodyLong, BodyShort, Tag } from "@navikt/ds-react";
 import React from "react";
 import useSWRImmutable from "swr/immutable";
 import { fetcher } from "../../api/api";
-import { antallVarslerUrl } from "../../api/urls";
+import { antallVarslerUrl, innboksUrl } from "../../api/urls";
 import styles from "./Innboks.module.css";
 
 const MeldingTag = ({ innbokser }: { innbokser: number }) => {
@@ -28,20 +28,20 @@ const Innboks = () => {
     return null;
   }
 
-  const type = innbokser > 0 ? "nyMelding" : "ingenNyMelding"
+  const type = innbokser > 0 ? "NyMelding" : "IngenNyMelding"
 
   return (
     <div className={styles.componentWrapper}>
       <div className={styles.container}>
-        <div className={`${styles.headerContainer} ${styles[`${type}`]}`}>
-          <BodyShort>Innboks</BodyShort>
+        <a className={`${styles.headerContainer} ${styles[`headerContainer${type}`]}`} href={innboksUrl}>
+          <BodyShort as="h2">Innboks</BodyShort>
           <div className={styles.tagChevron}>
             <MeldingTag innbokser={innbokser}/>
             <ChevronRightIcon className={styles.chevron} aria-hidden fontSize="24px" />
           </div>
-        </div>
+        </a>
 
-        <div className={`${styles.bodyContainer} ${styles[`${type}`]}`}>
+        <div className={`${styles.bodyContainer} ${styles[`bodyContainer${type}`]}`}>
           <BodyLong>
             Informasjon fra NAV og svar på henvendelser og referater fra samtaler du har på telefon, chat og “Skriv til
             oss”.
