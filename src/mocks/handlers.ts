ca9e6097f36440e107daeca7c9a4decdd34e6761
Atle Nordland <atle.nordland@nav.no>
2023-08-31 13:07:43 +0200

    dokumentarkiv-størrelsefix

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -27,18 +27,7 @@ export const sakerHandler = () => {
         ctx.status(200),
         ctx.json({
           sakstemaer: [
-            {
-              navn: "Serviceklager",
-              kode: "SER",
-              sistEndret: "2023-05-25T12:41:02Z",
-              detaljvisningUrl: "https://www.intern.dev.nav.no/mine-saker/tema/SER",
-            },
-            {
-              navn: "Arbeidsavklaringspenger",
-              kode: "AAP",
-              sistEndret: "2023-04-20T11:33:51Z",
-              detaljvisningUrl: "https://aap-innsyn.dev.nav.no/aap/mine-aap",
-            },
+            
           ],
           sakerURL: "https://www.dev.intern.nav.no/mine-saker",
           dagpengerSistEndret: null,
