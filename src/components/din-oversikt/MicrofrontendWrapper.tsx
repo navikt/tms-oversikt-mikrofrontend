0c559996b5c7c285f9bf9978853dec43cb0a3dec
Atle Nordland <atle.nordland@nav.no>
2023-10-02 12:24:18 +0200

    mf-skeleton

--- src/components/din-oversikt/MicrofrontendWrapper.tsx
+++ src/components/din-oversikt/MicrofrontendWrapper.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ErrorBoundary from "../../ErrorBoundary";
+import { Skeleton } from "@navikt/ds-react";
 
 const MicrofrontendWrapper = ({ manifestUrl }: { manifestUrl: string }) => {
   const Microfrontend = React.lazy(() => import(manifestUrl));
   return (
-    <React.Suspense fallback={null}>
+    <React.Suspense fallback={<Skeleton variant="rectangle" width={444} height={124} />}>
       <ErrorBoundary>
         <Microfrontend />
       </ErrorBoundary>
