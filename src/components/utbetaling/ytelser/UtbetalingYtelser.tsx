a6b5e4cf238f019bfac77025f8b47c398d01e9f9
Amirhossien Lotfi <amirhossien.lotfi@nav.no>
2023-10-17 19:33:59 +0200

    Hover state på link

--- src/components/utbetaling/ytelser/UtbetalingYtelser.tsx
+++ src/components/utbetaling/ytelser/UtbetalingYtelser.tsx
@@ -15,12 +15,12 @@ const UtbetalingYtelser = ({ ytelse, utbetaling, isKommende, id }: Props) => {
     <div className={styles.wrapper}>
       <a
         href={`${utbetalingsoversiktUrl}/utbetaling/${id}`}
-        className={`${styles.container} ${isKommende && styles.kommendeUtbetaling}`}
+        className={`${styles.container} ${isKommende ? styles.kommende : styles.utbetalt}`}
       >
         <div className={styles.ytelse}>
           <BodyShort>{ytelse}</BodyShort>
           <BodyShort weight="semibold" className={styles.ytelseSum}>{`${utbetaling.toLocaleString(
-            "no-nb"
+            "no-nb",
           )} kr`}</BodyShort>
         </div>
         <ChevronRightIcon className={styles.chevron} aria-hidden fontSize="24px" />
